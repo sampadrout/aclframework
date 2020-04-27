5d2c4dd9959ca4d1aad09c1dbeff6c5cbd49e79a
Sampad Rout <sampad.rout@gmail.com>
2020-04-27 15:37:57 +0530

    modified to ./gradlew

--- jenkins-pipelines/jenkinsfile.groovy
+++ jenkins-pipelines/jenkinsfile.groovy
@@ -4,7 +4,7 @@ node('acl-slave') {
         git 'https://github.com/sampadrout/aclframework.git'
     }
     stage('Build') { // for display purposes
-        sh "gradle clean build"
+        sh "/.gradlew clean build"
     }
     stage('Run') { // for display purposes
         sh "java -jar build/libs/Automation-1.0-SNAPSHOT.jar"

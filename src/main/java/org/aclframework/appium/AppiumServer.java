895a0f7b7048432ed291ff8a2114f7b639d7136a
sampadrout <sampad.rout@gmail.com>
2020-07-09 20:48:03 +0530

    WIP

--- src/main/java/org/aclframework/appium/AppiumServer.java
+++ src/main/java/org/aclframework/appium/AppiumServer.java
@@ -4,7 +4,7 @@ import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 import io.appium.java_client.service.local.flags.GeneralServerFlag;
 
-import java.io.File;
+import java.io.*;
 import java.io.IOException;
 
 import static org.aclframework.logger.LoggingManager.logMessage;
@@ -13,10 +13,23 @@ public class AppiumServer {
 
     public static AppiumDriverLocalService appium;
 
+    File appiumLogDir = new File(System.getProperty("user.dir") + "/appiumlogs");
+    appiumLogDir.
+    if (!appiumLogDir.exists()) {
+        appiumLogDir.mkdir();
+    }
+
+    File logFile = new File(appiumLogDir, "appiumLogs.txt");
+    if (logFile.exists()) {
+        logFile.delete();
+    }
+
     public static void start() throws IOException {
         AppiumServiceBuilder builder = new AppiumServiceBuilder()
                 .withAppiumJS(new File("/usr/local/lib/node_modules/appium/build/lib/main.js"))
-                .withArgument(GeneralServerFlag.SESSION_OVERRIDE);
+                .withArgument(GeneralServerFlag.SESSION_OVERRIDE)
+                .withLogFile(new File(System.getProperty("user.dir") + "/appiumlogs/appiumLogs.txt"));
+
         appium = builder.build();
         appium.start();
         logMessage("Appium server has been started");
--- src/main/java/org/aclframework/helpers/Page.java
+++ src/main/java/org/aclframework/helpers/Page.java
@@ -1,7 +1,15 @@
 package org.aclframework.helpers;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import io.appium.java_client.PerformsTouchActions;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.offset.PointOption;
+import static io.appium.java_client.touch.TapOptions.tapOptions;
+import static io.appium.java_client.touch.WaitOptions.waitOptions;
+import static io.appium.java_client.touch.offset.ElementOption.element;
+
+import org.openqa.selenium.*;
+
+import static java.time.Duration.ofSeconds;
 
 public class Page {
 
@@ -23,4 +31,23 @@ public class Page {
         return element.isDisplayed();
     }
 
+    public static void verticalScroll(WebDriver driver) {
+        Dimension size = driver.manage().window().getSize();
+        int start_Y = (int) (size.getHeight() * 0.75);
+        int end_Y = (int) (size.getHeight() * 0.10);
+        int start_end_X = (int) (size.getWidth() * 0.50);
+
+        TouchAction swipe = new TouchAction((PerformsTouchActions) driver).press(PointOption.point(start_end_X, start_Y))
+                .waitAction(waitOptions(ofSeconds(2)))
+                .moveTo(PointOption.point(start_end_X, end_Y)).release().perform();
+    }
+
+    public void tapElementAt(WebElement element, double xPct, double yPct, WebDriver driver) {
+        Rectangle elRect = element.getRect();
+        int xPoint = elRect.x + (int)(elRect.getWidth() * xPct);
+        int yPoint = elRect.y + (int)(elRect.getHeight() * yPct);
+
+        TouchAction tapAtPoint = new TouchAction((PerformsTouchActions) driver)
+                .tap(tapOptions().withElement(element(element, xPoint, yPoint))).perform();
+    }
 }
